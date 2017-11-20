7279224b3130a3d53c01983fdba5ca891e8da0dc
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-20 21:03:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
-def fizzbuzz(int)
-  if int % 3 === 0 && int % 5 === 0
+def fizzbuzz(num)
+  if num % 3 === 0 && num % 5 === 0
   return "FizzBuzz"
-elsif int % 5 === 0
+elsif num % 5 === 0
     return "Buzz"
-  elsif int % 3 === 0
+  elsif num % 3 === 0
    return  "Fizz"
  end
 end
